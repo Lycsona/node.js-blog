518ebd77e56b153c7c0c459797b63c55279013b4
Maria <maria.ostashevskaya@appfor.de>
2018-06-14 15:17:37 +0300

    update app

--- app.js
+++ app.js
@@ -1,51 +1,84 @@
-//Definitions
-var express = require('express')
-var cons = require('consolidate')
-var app = express()
-var expressLess = require('express-less');
+var http = require('http'),
+    path = require('path'),
+    methods = require('methods'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    cors = require('cors'),
+    passport = require('passport'),
+    errorhandler = require('errorhandler'),
+    mongoose = require('mongoose');
 
-// Set Mustache as the Template Engine
-app.engine('html', cons.mustache);
+var isProduction = process.env.NODE_ENV === 'production';
 
-// Set up Views and Partials
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+// Create global app object
+var app = express();
 
-// Set up Static Files
-app.use('/', express.static('assets'));
+app.use(cors());
 
-// Set up LESS
-app.use('/css', expressLess(__dirname + '/assets/less', { compress: true }));
+// Normal express config defaults
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Index Page
-app.get('/', function (req, res){
-	res.render('index', 
-	  {
-		title: 'Home',
-		partials : {
-			head : '/partials/head'
-		}
-	  });
+app.use(require('method-override')());
+app.use(express.static(__dirname + '/public'));
+
+app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false  }));
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if(isProduction){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect('mongodb://localhost/conduit');
+  mongoose.set('debug', true);
+}
+
+require('./models/User');
+require('./models/Article');
+require('./models/Comment');
+require('./config/passport');
+
+app.use(require('./routes'));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-	// All Errors Except for 404 Page.
-	app.use(function(err, req, res, next){
-		var requestedURL = req.protocol + '://' + req.get('Host') + req.url;	
-		console.error(err.stack);
-		console.log(err.stack + '  URL: ' + requestedURL);
-		res.render('error', {title: err.stack});
-	});
+/// error handlers
 
-	// 404 Error Page. MUST BE LAST (except for server).
-	app.use(function(req, res, next){
-		var requestedURL = req.protocol + '://' + req.get('Host') + req.url;
-		console.log('Error: 404 - ' + requestedURL );
-		res.render('error', {title: '404'});
-	});
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
 
-// Set the Server Up
-var server = app.listen(3000, function() {
-	var host = server.address().address
-	var port = server.address().port
-	console.log('App is listening at http://%s:%s', host, port)
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+// finally, let's start our server...
+var server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port);
 });
